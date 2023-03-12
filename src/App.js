59220834233e540398f7d2fa1d5c0bf363720665
AYUSH <ayushrajbls@gmail.com>
2023-03-12 15:04:48 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Navbar from './components/Navbar'
 import Landing from './components/Landing';
 import Article from './components/Article';
+import Footer from './components/Footer';
 function App() {
   return (
     <>
    <Navbar />
    <Landing/>
    <Article/>
+   <Footer/>
    </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <button className='border border-gray-600 text-gray-600 rounded-xl px-2 py-1 text-center flex justify-center mx-auto my-10 hover:bg-gray-200 '>View More</button>
+    </div>
+  )
+}
+
+export default Footer
