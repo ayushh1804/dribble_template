a308f0a5df10835386440883f00e85a6292390c4
AYUSH <ayushrajbls@gmail.com>
2023-03-12 16:56:55 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Navbar from './components/Navbar'
 import Landing from './components/Landing';
 import Article from './components/Article';
 import Footer from './components/Footer';
+import Middle from './components/Middle';
 function App() {
   return (
     <>
    <Navbar />
    <Landing/>
+   <Middle/>
    <Article/>
    <Footer/>
    </>
---
+++ src/components/Middle.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Middle = () => {
+    return (
+        <div className=''>
+            <div className='my-20 text-gray-500 font-bold text-lg'>
+                <ul className=' flex justify-around '>
+                    <li className='hover:underline decoration-slate-400 underline-offset-8 '>View All</li>
+                    <li className='hover:underline decoration-slate-400 underline-offset-8 '>NFT</li>
+                    <li className='hover:underline decoration-slate-400 underline-offset-8 '>BlockChain</li>
+                    <li className='hover:underline decoration-slate-400 underline-offset-8 '>BitCoin</li>
+                    <li className='hover:underline decoration-slate-400 underline-offset-8 '>DAO</li>
+                    <li className='hover:underline decoration-slate-400 underline-offset-8 '>DeFi</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Middle
