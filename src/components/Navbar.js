d13ae75970f561013a8b33f8f1f54f93c76cf7fd
AYUSH <ayushrajbls@gmail.com>
2023-03-12 14:22:09 +0530

    added navbar section

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import image1 from "../assets/blockchain.png"
+const Navbar = () => {
+  return (
+    <div>
+      <div className='bg-[#0C1E45] text-white text-center p-3'>
+        Blog is about...
+      </div>
+      <div className='flex justify-around mt-8'>
+        <span className='mt-4'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M16 18v2H5v-2h11zm5-7v2H3v-2h18zm-2-7v2H8V4h11z"/></svg></span>
+        <span><img src={image1} alt="" className='w-16'/></span>
+        <span> <button className='bg-[#0C1E45] p-3 rounded-3xl text-white'> Subscribe</button></span>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
